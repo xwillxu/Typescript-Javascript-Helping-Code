c552ab63797bab811ed918d1c56a12bb5b676c66
Xwill <89924712+xwillxu@users.noreply.github.com>
2023-01-27 18:44:14 -0500

    Update selectOutOfTwoTeams.ts

--- selectOutOfTwoTeams.ts
+++ selectOutOfTwoTeams.ts
@@ -1,1 +1,25 @@
+selectTeam(teamOneScore, teamTwoScore, teamOne, teamTwo, teamOneName, teamTwoName) {
+        let team = ""
+        if (teamOneScore < teamTwoScore) {
+            team = teamOneName
+        } else if (teamTwoScore < teamOneScore) {
+            team = teamTwoName
+        } else if (teamOneScore == teamTwoScore) {
+            const teamOnePlayers = teamOne.players
+            const teamTwoPlayers = teamTwo.players
+            if (teamOnePlayers < teamTwoPlayers) {
+                team = teamOneName
+            } else if (teamTwoPlayers < teamOnePlayers) {
+                team = teamTwoName
+            } else if (teamOnePlayers == teamTwoPlayers) {
+                const randomNumber = random(1, 2)
+                if (randomNumber == 1) {
+                    team = teamOneName
+                } else if (randomNumber == 2) {
+                    team = teamTwoName
+                }
+            }
+        }
 
+     return team
+}
